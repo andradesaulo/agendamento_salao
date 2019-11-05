943efe1ed053cffc63c3e75fd1c353e10fc8cb73
Saulo Andrade <sauloandrade0809@gmail.com>
2019-11-05 18:01:31 -0300

    testando

--- frontend/src/main/App.jsx
+++ frontend/src/main/App.jsx
@@ -1,10 +1,18 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-import React from 'react'
-import './App.css'
-import { BrowserRouter } from 'react-router-dom'
 
+import React from 'react'
+import { ThemeProvider } from '@material-ui/core/styles'
+import { BrowserRouter } from 'react-router-dom'
+import { createMuiTheme } from '@material-ui/core/styles';
 import Routes from './Routes'
+import red from '@material-ui/core/colors/red';
+const theme = createMuiTheme({
+    palette: {
+        primary: red;
+    },
+})
 export default props =>
-    <BrowserRouter>
-        <Routes/>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+        <BrowserRouter>
+            <Routes/>
+        </BrowserRouter>
+    </ThemeProvider>
